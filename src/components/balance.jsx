244e4dc03cce65b0d506f2415409f656c54a4dd2
salman-abdulqadir <salmanabdulkadir82@gmail.com>
2022-12-14 21:17:29 +0400

    chart component is not working

---
+++ src/components/balance.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//image
+import logo from "../images/logo.svg";
+const Balance = ({balance}) => {
+    return(
+        <div className="balance-container">
+            <div className="balance">
+                <p>My Balance</p>
+                <h2 id="balance-amount">${balance}</h2>
+            </div>
+            <img src={logo} alt="logo"/>
+        </div>
+    )
+}
+
+export default Balance;
---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Salman Abdulqadir</a>.
+    </footer>
+  );
+};
